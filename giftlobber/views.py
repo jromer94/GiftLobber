e0a5a5be364c3c0062cd5a67dfe2de83b7b18592
Jromer94 <Jromer94@gmail.com>
2014-01-18 07:24:29 -0500

    added login function and way to add contacts

--- giftlobber/views.py
+++ giftlobber/views.py
@@ -1,15 +1,17 @@
 from giftlobber import app
-from flask import render_template, request
+from flask import render_template, request, flash, session, redirect, url_for
 from flask.ext.pymongo import PyMongo
+from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://admin:admin@linus.mongohq.com:10015/GiftLobber')
+client = MongoClient('mongodb://admin2:admin@linus.mongohq.com:10015/GiftLobber')
+client = client.GiftLobber
 contacts = {}
 jobs = {}
 
-@app.route('/?contact')
+@app.route('/contacts')
 def listContacts():
-    return contacts
+    return "temp"
 
 
 @app.route('/?job')
@@ -24,5 +26,32 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-    
+	print request.form['username']
+   	user = client.users.find_one({'name': request.form['username']})
+        print user
+        if user and  user['pass'] == request.form['pass']:
+            session['logged_in'] = True
+            session['user'] = user['name']
+            flash('You were logged in')
+            return redirect(url_for('index'))
+        else:
+            flash('Incorrect login info')
+ 
     return render_template('landing2.html')   
+
+@app.route('/contacts/add', methods['GET', 'POST'])
+def addContact():
+    if request.method == 'POST':
+        client.contacts.insert({"first": request.form['first'],
+            "last": request.form['last'],
+            "address1": request.form['address1'],
+            "address2": request.form['address2'],
+            "city": request.form['city'],
+            "state": request.form["state"],
+            "zip": request.form["zip"],
+            "country": "US"})
+        return redirect(url_for('index')    
+
+    return render_template('addContact.html')
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
