aca019d4f8fdbe9198a452e4a1a38e9f2bbfd66d
Jromer94 <Jromer94@gmail.com>
2014-01-18 06:01:22 -0500

    started login feature

--- giftlobber/views.py
+++ giftlobber/views.py
@@ -1,6 +1,9 @@
 from giftlobber import app
 from flask import render_template, request
+from flask.ext.pymongo import PyMongo
 
+
+client = MongoClient('mongodb://admin:admin@linus.mongohq.com:10015/GiftLobber')
 contacts = {}
 jobs = {}
 
@@ -17,3 +20,9 @@ def listJobs():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+    
+    return render_template('landing2.html')   
