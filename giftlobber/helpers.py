8c00af0161c43282fdaaf3782ce0df853789a82b
Jromer94 <Jromer94@gmail.com>
2014-01-19 00:14:21 -0500

    fixed some stuff

--- giftlobber/helpers.py
+++ giftlobber/helpers.py
@@ -1,11 +1,18 @@
 import urllib
 import urllib2
 import json
+import base64
 
 
-def lobPost(url, values):
+def lobPost(url, values, password):
+	base64string = base64.encodestring('%s:' % (password))[:-1]
+	print "fuckkkk"	
+	data = urllib.urlencode(values)
+	req = urllib2.Request(url)
+	print data
+	req.add_data(urllib.urlencode(values))
+	req.add_header("Authorization", "Basic %s" % base64string)
+	response = urllib2.urlopen(req)
 
-    data = urllib.urlencode(values)
-    req = urllib2(url, data)
-    response = urllib2.urlopen(req)
-    return json.load(response)
+
+	return json.load(response)
